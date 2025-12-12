2f147865310d8d04c1dc84402b80bad48f7f3ee7
Conroy Makhubu <cmakhubu@gmail.com>
2025-12-12 17:08:13 +0200

    update index.html index.js

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ let paragraph1 = document.getElementById("getpara1");
 let paragraph2 = document.getElementById("getpara2");
 
 
-
-function updateScore1(){
- console.log("Updating Score 1...");
-
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+function updateGroup1Score1(){
+ 
+ count1 += 1;
+ paragraph1.textContent = count1;
 }
 
-function updateScore2(){
+function updateGroup1Score2(){
+  console.log("Updated Score 2");
+}
+
+function updateGroup1Score3(){
+  console.log("Updated Score 3");
+}
+
+
+function updateGroup2Score1(){
+ 
+ count1 += 1;
+ paragraph1.textContent = count1;
+}
+
+function updateGroup2Score2(){
   console.log("Updated Score 2");
 }
 
 function updateScore3(){
   console.log("Updated Score 3");
-}
