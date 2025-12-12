f32febb7cabdf6f68bc55be454f3219639eeda80
Conroy Makhubu <cmakhubu@gmail.com>
2025-12-12 16:54:21 +0200

    update index.html with click events and update index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let paragraph1 = document.getElementById("getpara1");
+let paragraph2 = document.getElementById("getpara2");
+
+
+
+function updateScore1(){
+ console.log("Updating Score 1...");
+
+}
+
+function updateScore2(){
+  console.log("Updated Score 2");
+}
+
+function updateScore3(){
+  console.log("Updated Score 3");
+}
